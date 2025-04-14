918f373de32a29cae9f0d99726da0309275f4b5b
janetjk <janetjk@metropolia.fi>
2025-04-14 10:53:25 +0300

    p

--- MODUULI_2/MOD2_5/mod2_5.js
+++ MODUULI_2/MOD2_5/mod2_5.js
@@ -1,12 +1,19 @@
-'use strict';
+'use strict'
+let ask = true
+let numbers = []
+let number = parseInt(prompt('Give me a number'))
+numbers.push(number)
+while (ask) {
+  number = parseInt(prompt('Give me a number'))
+  if (numbers.includes(number)) {
+    ask = false
+    console.log('Number already entered. Ending program. Numbers entered: ' + numbers.sort((a,b) => a-b))
+  }
+  else {
+    numbers.push(number)
+  }
 
-let numbers = [];
-let number = parseInt(prompt('Give me a number'));
-if (numbers.includes(number)) {
-  console.log("Number already on list" + numbers.sort().reverse())
-}
-else {
-  numbers.push(number)
+
 }
 
 
